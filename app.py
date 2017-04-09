9e80002c7a7c2751d09f02ab3b28d6142eaedcd1
Abhishek Pratap Singh Sikarwar <apsingh@quadanalytix.com>
2017-04-10 00:08:56 +0530

    push

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os,sys
 
 
@@ -20,29 +21,35 @@ def verify():
     return "Hello world", 200
 
 @app.route('/' , methods= ['POST'])
-def post_req():
+def webhook():
+
 
     data = request.get_json()
-
-    if data['object'] == 'page':
-        for entry in data['entry']:
-            for messaging_event in entry['messaging']:
-
-                sender_id = messaging_event['sender']['id']
-                recipient_id = messaging_event['recipient']['id']
-
-                if messaging_event.get('message'):
-                    if 'text' in messaging_event['message']:
-                        messaging_text = messaging_event['message']['text']
-                    else:
-                        messaging_text = 'no text'
-
-                    response = messaging_text
-                    bot.send_text_message(sender_id, response)
+    log(data)
+    #
+    # if data['object'] == 'page':
+    #     for entry in data['entry']:
+    #         for messaging_event in entry['messaging']:
+    #
+    #             sender_id = messaging_event['sender']['id']
+    #             recipient_id = messaging_event['recipient']['id']
+    #
+    #             if messaging_event.get('message'):
+    #                 if 'text' in messaging_event['message']:
+    #                     messaging_text = messaging_event['message']['text']
+    #                 else:
+    #                     messaging_text = 'no text'
+    #
+    #                 response = messaging_text
+    #                 bot.send_text_message(sender_id, response)
 
     return 'yes',200
+
+def log(message):
+    print(message)
+    sys.stdout.flush()
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port = 8001)
+    app.run(debug=True,port = 80)
